40307edd92285b0101943c57d041328b9154ab4d
hgq0916 <1286908118@qq.com>
2020-07-24 09:20:04 +0800

    重写重画方法

--- tank-01/src/main/java/com/mashibing/tank/TankFrame.java
+++ tank-01/src/main/java/com/mashibing/tank/TankFrame.java
@@ -7,6 +7,8 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
+  int x=200,y=200;
+
   public TankFrame(){
     setSize(800,600);
     setResizable(false);
@@ -22,6 +24,8 @@ public class TankFrame extends Frame {
 
   @Override
   public void paint(Graphics g) {
-    g.fillRect(200,200,50,50);
+    g.fillRect(x,y,50,50);
+    x +=10;
+    y +=10;
   }
 }
