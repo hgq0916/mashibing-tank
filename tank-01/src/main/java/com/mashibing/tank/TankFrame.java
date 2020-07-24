5c688a953ca9f9ffb36b894c927125ec027e88a3
hgq0916 <1286908118@qq.com>
2020-07-24 22:18:03 +0800

    添加坦克静止的处理，只有四个键都没按就是静态的

--- tank-01/src/main/java/com/mashibing/tank/TankFrame.java
+++ tank-01/src/main/java/com/mashibing/tank/TankFrame.java
@@ -67,14 +67,15 @@ public class TankFrame extends Frame {
 
     private void setMainTankDir() {
 
-      mytank.setMoving(true);
-
-      if(BU) mytank.setDir(Dir.UP);
-      if(BD) mytank.setDir(Dir.DOWN);
-      if(BL) mytank.setDir(Dir.LEFT);
-      if(BR) mytank.setDir(Dir.RIGHT);
-
-      if(!BU && !BD && !BL && !BR) mytank.setMoving(false);
+      if(!BU && !BD && !BL && !BR){
+        mytank.setMoving(false);
+      } else {
+        mytank.setMoving(true);
+        if(BU) mytank.setDir(Dir.UP);
+        if(BD) mytank.setDir(Dir.DOWN);
+        if(BL) mytank.setDir(Dir.LEFT);
+        if(BR) mytank.setDir(Dir.RIGHT);
+      }
 
     }
 
