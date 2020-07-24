078eefcaef0fff977299a11c4dde54e676ae638f
hgq0916 <1286908118@qq.com>
2020-07-24 21:00:54 +0800

    增加键盘监听事件

--- tank-01/src/main/java/com/mashibing/tank/T.java
+++ tank-01/src/main/java/com/mashibing/tank/T.java
@@ -1,9 +1,5 @@
 package com.mashibing.tank;
 
-import java.awt.Frame;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class T {
 
   public static void main(String[] args) {
--- tank-01/src/main/java/com/mashibing/tank/TankFrame.java
+++ tank-01/src/main/java/com/mashibing/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.mashibing.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -14,6 +16,7 @@ public class TankFrame extends Frame {
     setResizable(false);
     setTitle("tank war");
     setVisible(true);
+    this.addKeyListener(new MyKeyListener());
     addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
@@ -24,8 +27,24 @@ public class TankFrame extends Frame {
 
   @Override
   public void paint(Graphics g) {
+    //paint会先清屏，然后绘制
+    System.out.println("paint");
     g.fillRect(x,y,50,50);
     x +=10;
     y +=10;
+  }
+
+  private class MyKeyListener extends KeyAdapter {
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+      System.out.println("keyPressed");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+      System.out.println("keyReleased");
+    }
   }
 }
