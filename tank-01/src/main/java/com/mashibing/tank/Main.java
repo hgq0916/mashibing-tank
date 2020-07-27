60788f2395f4bbd6c7251dd7e7826847491226d5
hgq0916 <1286908118@qq.com>
2020-07-27 12:43:39 +0800

    加入背景音乐

--- tank-01/src/main/java/com/mashibing/tank/Main.java
+++ tank-01/src/main/java/com/mashibing/tank/Main.java
@@ -5,6 +5,10 @@ public class Main {
   public static void main(String[] args) {
     TankFrame tankFrame = new TankFrame();
 
+    new Thread(()->{
+      new Audio("audio/war1.wav").loop();
+    }).start();
+
     while (true){
       try {
         Thread.sleep(50);
